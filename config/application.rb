c2368eec69558bceecc7eb2a867ef7c06013dbae
Greg Rogers <sregorg@gmail.com>
2013-09-10 19:52:30 -0400

    Tweak to reduce Heroku deploy errors.

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,7 @@ module BarGameOlympics
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false # recommended by Heroku
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
