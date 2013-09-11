2a0abe5417dfb1277494d01f38c9fd8244d973c7
Greg Rogers <sregorg@gmail.com>
2013-09-11 17:51:09 -0400

    Adds venue model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130911121629) do
+ActiveRecord::Schema.define(:version => 20130911214950) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -60,6 +60,12 @@ ActiveRecord::Schema.define(:version => 20130911121629) do
   end
 
   create_table "sports", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "venues", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
