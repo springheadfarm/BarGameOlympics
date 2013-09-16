73a08d65fd5f53369761ec66d41912b97725754a
Greg Rogers <sregorg@gmail.com>
2013-09-16 05:06:18 -0400

    Another attempt.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ BarGameOlympics::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( jbclock.js jquery.tools.min.js app/assets/images/ticket_packages/* )
+  config.assets.precompile += %w( jbclock.js jquery.tools.min.js *.gif )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
