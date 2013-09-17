ea9dce526985c55b6cec56e297db2f517402bedb
Greg Rogers <sregorg@gmail.com>
2013-09-17 06:48:47 -0400

    Gave up.  Moved images out of subdirectory to app/assets/images.

--- app/helpers/info_helper.rb
+++ app/helpers/info_helper.rb
@@ -2,7 +2,7 @@ module InfoHelper
 
 	 def ticket_package(image, &block)
 	 	content_tag(:div, :class=>"item") do
-	 		image_tag(image) +
+	 		image_tag("ticket_package_#{image}") +
 	 		markdown(capture(&block).strip)
 	 	end
 	 end
