358490a76e202e4adb0c8af82fd771bee7afa5fe
Greg Rogers <sregorg@gmail.com>
2015-03-15 18:48:14 -0400

    Adds new countdown.  Closes #94.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,9 +49,9 @@ BarGameOlympics::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( jbclock.js jquery.tools.min.js jquery.colorbox.js)
+  config.assets.precompile += %w( flipclock.js jquery.tools.min.js jquery.colorbox.js)
   config.assets.precompile += %w( tablesorter/jquery.tablesorter.min.js tablesorter/jquery.tablesorter.widgets.min.js)
-  config.assets.precompile += %w( tablesorter/theme.ice.css stats.js medals_heatmap.js)
+  config.assets.precompile += %w( tablesorter/theme.ice.css stats.js medals_heatmap.js flipclock.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
