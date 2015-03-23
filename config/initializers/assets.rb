e4ad446f52ef415f934b2c2f35e7980b02ea4cba
Greg Rogers <sregorg@gmail.com>
2015-03-23 08:00:24 -0400

    Missed some changes

---
+++ config/initializers/assets.rb
@@ -0,0 +1,8 @@
+# Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+# This used to be in production.rb.  New Rails 4.2 functionality?
+
+Rails.application.config.assets.precompile += %w( flipclock.js )
+
+Rails.application.config.assets.precompile += %w( galleries.js )
+Rails.application.config.assets.precompile += %w( galleria-1.4.2.js )
+Rails.application.config.assets.precompile += %w( galleria.twelve.min.js )
